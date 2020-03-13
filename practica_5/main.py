42545ee6924e9d7e2baaefd4c15d8eb5ef4b2369
Yonathan Lopez Mejia <yonathan@josie>
2020-03-13 10:35:08 -0500

    Triki finalizado con comentarios

--- practica_5/main.py
+++ practica_5/main.py
@@ -1,7 +1,25 @@
 import triqui as tr
-import sys
-import os
+import os 
+""" El módulo os se utiliza para obtener 
+la variable date del sistema *nix"""
 
+"""
+Nombre: Yonathan López Mejía
+Documento: 1017220389
+Variables adicionales:
+    * player_name = nombre del jugador humano
+    * user_win = contador para cuando el usuario gana
+    * computer_win = contador para cuando el ordenador gana
+    * tied_game = contador para cuando se da un empate
+    * tablero = lista con 10 posiciones donde se almacenan
+    las jugadas
+    * marca_usuario: almacena el símbolo que usará el usuario para marcar el tablero
+    * marca_computadora: almacena el símbolo que usará la computadora para marcar el tablero
+    * finalizar: esta variable booleana almacena True si se va a terminar
+    el programa y False si todavía se ejecutará al menos una vez más otra partida
+    * file: variable que tiene la referencia en memoria al archivo de texto a escribir
+    
+"""
 tr.printIntro('intro.txt')
 player_name = input('Nombre del jugador: \n')
 user_win = 0
@@ -10,22 +28,47 @@ tied_game = 0
 
 turn ="" # Indica quién tiene el turno para jugar, el usuario o la computadora.
 while True:
-    
+    """
+    En esta primera parte del ciclo principal se crea el tablero,
+    se definen las marcas del usuario y la computadora y se muestran en consola;
+    se elije el turno para saber quién empieza y se inicia
+    el juego. Además, se muestra el marcador actual
+    """
+    print('''
+    Marcador:
+        Victorias humanas:     {0}
+        Victorias computadora: {1}
+        Empates:               {2}
+    '''.format(user_win, computer_win, tied_game))
     tablero = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
     marca_usuario, marca_computadora = tr.inputPlayerLetter().split(',')
     print('La marca del usuario es:', marca_usuario)
     print('La marca de la computadora es:',marca_computadora)
-    #4. Quién va primero el usuario o la computadora?
     turn = tr.whoGoesFirst()
     print(turn + ' va primero.')
-    jugando = True # El juego ha iniciado
+    jugando = True
 
     while jugando:
-        if turn == 'Usuario': # 5. Turno del usuario
-
-            # a. Mostrar tablero
+        if turn == 'Usuario':
+            """
+            Cuando el turno lo tiene el usuario se ejecuta esta parte en
+            este orden:
+                1. Se muestra el tablero en el estado actual
+                2. Se actualiza el tablero con las siguientes condiciones
+                    *se recibe el tablero actualizado por parte
+                    de la función getPlayerMove en el módulo triqui
+                    * Se valida si el jugador ganó con la jugada anterior,
+                    en ese caso se muestra el tablero, se cuenta una victoria
+                    para el humano, se muestra un mensaje de victoria y se termina la partida
+                    *Si no, se valida si hay un empate preguntando si ya no hay
+                    espacios disponibles en el tablero, en caso de darse
+                    muestra el estado final del tablero, se cuenta un empate más
+                    se muestra un mensaje de empate y se termina la partida.
+                   * Si no hay victoria o empate entonces el computador
+                   toma el siguiente turno
+            """
+            
             tr.drawBoard(tablero)
-            # c. Actualizar el tablero
             tablero = tr.getPlayerMove(tablero, marca_usuario)
             if tr.isWinner(tablero, marca_usuario):
                 tr.drawBoard(tablero)
@@ -38,22 +81,25 @@ while True:
                 print('Empate')
                 break
         
-            # d. Verificar si el usuario ha ganado el juego.
-            #    Si si, mostrar tablero, mostrar mensaje de felicitación y terminar el juego.
-
-            # e. Verificar si hay empate.
-            #    Si si, mostrar tablero, mostar mensaje de empate y terminar el juego.
-
-            # f. Si el usuario no ha ganado y no hay empate, la computadora
-            #    toma el siguiente turno
-
             turn = 'Computadora'
 
-        else: # 6. Turno de la computadora.
+        else: 
+            """
+            Cuando la computadora tiene el turno se ejecuta esta parte
+            1. Se actualiza el tablero con el retorno de la función
+            getComputerMove() del módulo triki y se verifican las siguientes condiciones:
+                * Si la jugada anterior fue la ganadora entonces
+                se muestra el estado final del tablero, se cuenta 
+                una victoria para la computadora, se muestra un mensaje
+                de victoria y se termina la partida.
+                * Si no, se valida si hay espacios vacíos en el tablero,
+                en caso de que no hayan significa que hay un empate, por tanto,
+                se muestra el estado final del tablero, se cuenta un empate más
+                y se muestra un mensaje de empate
+                * En caso de que no haya jugada ganadora, ni empate, entonces
+                el jugador humano toma el turno
+            """
             tablero = tr.getComputerMove(tablero, marca_computadora)
-            # a. Computadora hace jugada.
-            # b. Actualizar el tablero.
-            # c. Verificar si la computadora ha ganado el juego.
             if tr.isWinner(tablero, marca_computadora):
                 tr.drawBoard(tablero)
                 computer_win += 1
@@ -64,21 +110,22 @@ while True:
                 tied_game += 1
                 print('Empate')
                 break
-                
-            
-           
-            #    Si si, mostrar tablero, mostrar mensaje indicando al usuario que ha perdido y terminar el juego.
 
-            # d. Verificar si hay empate.
-            #    Si si, mostrar tablero, mostar mensaje de empate y terminar el juego.
-
-            # f. Si la computadora no ha ganado y no hay empate, el usuario
-            #    toma el siguiente turno.
+         
             turn = 'Usuario'
             
 
-    # 7. Preguntar si el usuario quiere jugar una vez mas
     while True:
+        """Cuando se termina una partida se ejecuta la siguiente parte:
+       Se pregunta si quiere volver a jugar.
+        * En caso de querer volver a jugar se actualiza la variable
+        finalizar con el valor False y comienza una nueva partida para el mismo jugador
+        * En caso de querer terminar con el programa se escribe
+        en un archivo de texto externo el formato definido en la guía,
+        se escribe: la fecha en formato del sistema, el nombre del usuario,
+        la cantidad de victorias del usuario, de la máquina y el número de empates.
+        Finalmente se actualiza la variable finalizar con True y se rompe el ciclo
+    """
         opc = input('¿Quiere volver a jugar? s/n\n')
         if opc == 'n':
             with open('estadisticas.txt', 'a') as file:
@@ -97,9 +144,19 @@ while True:
                 '''.format(os.popen('date').read(), player_name, user_win, computer_win, tied_game)
                 file.write(message)
                 
-            sys.exit()
+            finalizar = True
+            break
         if opc == 's':
+            finalizar = False
             break
         print('Opción no válida')
-            
-    #    Si no, finalizar el programa.
+    """
+    En caso de querer terminar el juego luego de romper el ciclo principal
+    se valida el valor de variable finalizar. Si la variable es True
+    significa que se quiere acabar con todo el programa y se rompe el ciclo principal,
+    en caso de que la variable tenga el valor False significa que no se quiere
+    terminar la partida y se ignora.
+    """
+    if finalizar:
+        break
+
--- practica_5/triqui.py
+++ practica_5/triqui.py
@@ -1,5 +1,9 @@
 import random
 
+"""
+Nombre: Yonathan López Mejía
+Documento: 1017220389
+"""
 
 def printIntro(introFile):
     '''
@@ -28,15 +32,14 @@ def printIntro(introFile):
 
     with open(introFile) as message:
         for line in message:
-            print(line)
+            print(line, end='')
     
 
 def drawBoard(board):
-    # Esta función imprime el tablero en la consola
-    # Argumentos:
-    # Board: Lista de strings que representa el estado del tablero
+    """ Esta función imprime el tablero en la consola
+     Argumentos:
+     Board: Lista de strings que representa el estado del tablero """
 
-    # Desarrolle el cuerpo de la función aquí...
     print(''' 
      {6}  | {7}  | {8}
     ----+----+---
@@ -46,6 +49,12 @@ def drawBoard(board):
     '''.format(*board[1:]))
 
 def inputPlayerLetter():
+    """
+    Esta función le permite escoger al usuario entre la letra "X" y la letra "O".
+    retorna una lista de strings donde la letra escogida por el usuario
+    ocupa la primera posición y la letra que le corresponde a la computadora
+    ocupa la segunda posición.
+    """
     while True:
         opc = input('Seleccione X o O para jugar\n').upper()
         if opc == 'X':
@@ -58,35 +67,45 @@ def inputPlayerLetter():
             
         return letras
     
-    # Esta función le permite escoger al usuario entre la letra "X" y la letra "O".
-
-    # retorna una lista de strings donde la letra escogida por el usuario
-    # ocupa la primera posición y la letra que le corresponde a la computadora
-    # ocupa la segunda posición.
+    
 
     # Desarrolle el cuerpo de la función aquí...
     
 def whoGoesFirst():
-    # Esta función escoge de forma aleatoria quien inicial el juego.
-
-    # Retorna el string "Usuario" si el usuario inicia el juego o
-    # el string "Computadora" si la computadora inicia el juego.
-
-    # Desarrolle el cuerpo de la función aquí...
+    """
+    Esta función escoge de forma aleatoria quien inicial el juego.
+    Retorna el string "Usuario" si el usuario inicia el juego o
+    el string "Computadora" si la computadora inicia el juego.
+    """
     return random.choice(['Usuario', 'Computadora'])
 
 def makeMove(board, letter, move):
+    """
+    Esta función agrega la marca del jugador o la computadora en la lista
+    que representa el estado actual del tablero y luego
+    retorna el tablero
+    Argumentos:
+    board: Lista de strings que almacena el estado del tablero.
+    letter: Es la marca que se desea poner en el tablero ("X" o "O").
+    move: Es el número de la casilla donde se desea poner la marca.
+    """
+    
     board[move] = letter
     return board
 
-    # Argumentos:
-    # board: Lista de strings que almacena el estado del tablero.
-    # letter: Es la marca que se desea poner en el tablero ("X" o "O").
-    # move: Es el número de la casilla donde se desea poner la marca.
-
-    # Desarrolle el cuerpo de la función aquí...
 
 def isWinner(board, letter):
+    
+    """
+    Esta función verifica si hay una jugada ganadora en el tablero.
+    Valida cada una de las posibles formas de ganar definidas
+    por las combinaciones en las posiciones de la lista
+    Argumentos:
+    board: Lista de strings que almacena el estado del tablero.
+    letter: La marca que se desea verificar ("X" o "O").
+    Esta función retorna el valor lógico True, si hay una jugada ganadora o
+    retorna el valor lógico False, si no hay una jugada ganadora.
+    """
     if board[1:4] == [letter, letter, letter]:
         return True
     
@@ -112,32 +131,41 @@ def isWinner(board, letter):
         return True
     
     return False
-    # Esta función debe verificar si hay una jugada ganadora en el tablero.
-
-    # Argumentos:
-    # board: Lista de strings que almacena el estado del tablero.
-    # letter: La marca que se desea verificar ("X" o "O").
-
-    # Esta función debe retornar el valor lógico True, si hay una jugada ganadora o
-    # debe retornar el valor lógico False, si no hay una jugada ganadora.
-
-    # Desarrolle el cuerpo de la función aquí...
-    pass
+   
+ 
 
 def isSpaceFree(board, move):
+    """
+    Esta función verifica si hay una casilla vacía 
+    en una posición de la lista que representa el tablero.
+    Argumentos:
+    board: Lista de strings que almacena el estado del tablero.
+    move: Es el número de la casilla que se desea verificar.
+
+    Esta función retorna el valor lógico True, si la casilla está vacía
+    en caso contrario, retorna el valor lógico False.
+    """
     return board[move].isspace()
-    # Esta función verifica si hay una casilla vacía en el tablero.
-
-    # Argumentos:
-    # board: Lista de strings que almacena el estado del tablero.
-    # move: Es el número de la casilla que se desea verificar.
-
-    # Esta función debe retornar el valor lógico True, si la casilla está vacía
-    # en caso contrario, debe retornar el valor lógico False.
-
-    # Desarrolle el cuerpo de la función aquí...
+    
 
 def getPlayerMove(board, marca_usuario):
+    
+    """
+    Esta función le pide al usuario que ingrese el número de la casilla
+    que quiere marcar definida por un número entre 1 y 9.
+    Se pide el número y se valida usando la función isSpaceFree si el tablero
+    en esa posición está vacío, en caso de estar vacío
+    envía el estado del tablero, la marca del usuario y la posición
+    a la función makeMove, en caso de que no lo esté muestra un mensaje
+    y vuelve a pedir ingresar la información. El ciclo
+    se repite hasta que el usuario ingrese el número correcto
+    
+    Argumentos:
+    board: Lista de strings que almacena el estado del tablero.
+    marca_usuario: el símbolo que representa la marca del usuario
+    en la partida actual.
+    Esta función retorna el tablero con el movimiento ejecutado.
+    """
     while True:
         numero = int(input('Escriba el número de la casilla (1-9): '))
         if 1 <= numero <= 9:
@@ -150,30 +178,20 @@ def getPlayerMove(board, marca_usuario):
             print('Digite un número entre 1 y 9')
     
     return makeMove(board, marca_usuario, numero)
-    # Esta función le pide al usuario que ingrese el número de la casilla
-    # que quiere marcar.
-
-    # Argumentos:
-    # board: Lista de strings que almacena el estado del tablero.
-
-    # Esta función retorna el número de la casilla seleccionada por el usuario.
-
-
-    # Desarrolle el cuerpo de la función aquí...
-
+    
+ 
 def chooseRandomMoveFromList(board, movesList):
     
-    # Esta función escoge de forma aleatoria una casilla vacía del tablero.
-
-    # Argumentos:
-    # board: Lista de strings que almacena el estado del tablero.
-    # moveList: Lista que contiene los números de las casillas a verificar (ver documento de la práctica).
-
-    # Esta función debe retornar alguno de los números de casillas en moveList
-    # desde que dicha casilla esté vacía. Si ninguna de las casillas está vacía,
-    # esta función debe retornar el valor None.
-
-    # Desarrolle el cuerpo de la función aquí...
+    """
+    Esta función escoge de forma aleatoria una casilla vacía del tablero.
+    Argumentos:
+    board: Lista de strings que almacena el estado del tablero.
+    moveList: Lista que contiene los números de las casillas a verificar.
+    Esta función retorna alguno de los números de casillas en moveList
+    desde que dicha casilla esté vacía. Si ninguna de las casillas está vacía,
+    esta función retorna el valor None.
+    """
+    
     while len(movesList) > 0:
         move = random.choice(movesList)
         if isSpaceFree(board, move):
@@ -185,7 +203,25 @@ def chooseRandomMoveFromList(board, movesList):
 
 def getComputerMove(board, computerLetter):
     
-    copy_of_board = board
+    
+    """
+    Esta función hace una copia del tablero e implementa la estrategia de juego de la computadora.
+    1. Verifica si la computadora puede ganar, en caso afirmativo realiza la jugada y retorna el tablero
+    con la jugada
+    2. Si no, verifica si el usuario puede ganar en la siguiente jugada, en caso de que 
+    sí, bloquea la jugada del usuario y retorna el tablero actualizado
+    3. Si no, trata de poner una marca en alguna de las esquinas
+    4. Si no, trata de marcar la casilla del centro
+    5. Si no, tratar de poner una marca en alguna de las casillas de los lados
+    si se cumple 3, 4, o 5 o ninguna, igualmente retorna el tablero con la jugada
+    o sin ella si no puede realizarla
+    
+    Argumentos:
+    board: Lista de strings que almacena el estado del tablero.
+    computerLetter: La marca que está usando la computadora.
+    """
+    
+    copy_of_board = board #Copia del estado del tablero
     i = 1
     while i < len(copy_of_board):
         if isSpaceFree(copy_of_board, i):
@@ -224,34 +260,15 @@ def getComputerMove(board, computerLetter):
        
     return board
 
-    # Esta función implementa la estrategia de juego de la computadora.
-
-    # Argumentos:
-    # board: Lista de strings que almacena el estado del tablero.
-    # computerLetter: La marca que está usando la computadora.
-
-    # Desarrolle el cuerpo de la función aquí...
-
-    # 1. Verificar si la computadora puede ganar...
-
-    # 2. Si no, verificar si el usuario puede ganar en la siguiente jugada, si si, bloquear esta jugada...
-
-    # 3. Si no, tratar de poner una marca en alguna de las esquinas, si alguna está vacía...
-
-    # 4. Si no, tratar de marcar la casilla del centro, si esta está vacía...
-
-    # 5. Si no, tratar de poner una marca en alguna de las casillas de los lados...
 
 
 def isBoardFull(board):
+    """
+    Esta función verifica si el tablero está lleno.
+    Argumentos:
+    board: Lista de strings que almacena el estado del tablero.
+    Esta función retorna el valor lógico True, si el tablero está lleno.
+    En caso contrario retorna el valor lógico False.
+    """
     return ' ' in board[1:]
-    # Esta función verifica si el tablero está lleno.
-
-    # Argumentos:
-    # board: Lista de strings que almacena el estado del tablero.
-
-    # Esta función debe retorna el valor lógico True, si el tablero está lleno.
-    # En caso contrario debe retornar el valor lógico False.
-
-    # Desarrolle el cuerpo de la función aquí...
     
